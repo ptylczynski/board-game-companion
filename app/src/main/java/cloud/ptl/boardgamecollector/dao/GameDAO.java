39fd79bfa2b88b4020ecd2a0ecca4d373852be86
Piotr Tylczynski <piotr@ptl.cloud>
2021-05-24 23:08:12 +0200

    game dao add

--- app/src/main/java/cloud/ptl/boardgamecollector/dao/GameDAO.java
+++ app/src/main/java/cloud/ptl/boardgamecollector/dao/GameDAO.java
@@ -3,11 +3,33 @@ package cloud.ptl.boardgamecollector.dao;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import java.util.List;
 
 import cloud.ptl.boardgamecollector.entity.Game;
 
 @Dao
 public interface GameDAO {
+    @Transaction
     @Insert
     void insertAll(Game... games);
+
+    @Transaction
+    @Update
+    void updateAll(Game... games);
+
+    @Transaction
+    @Delete
+    void delete(Game game);
+
+    @Transaction
+    @Query("SELECT * FROM Game")
+    List<Game> selectAll();
+
+    @Transaction
+    @Query("SELECT * FROM Game WHERE gameId=:id")
+    Game findById(int id);
 }
