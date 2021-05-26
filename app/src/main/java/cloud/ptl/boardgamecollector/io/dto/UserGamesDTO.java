d1542f3d021aaf513c2b9bae7ea955c1758f5b0e
Piotr Tylczynski <piotr@ptl.cloud>
2021-05-27 01:06:05 +0200

    user games parser add

---
+++ app/src/main/java/cloud/ptl/boardgamecollector/io/dto/UserGamesDTO.java
@@ -0,0 +1,12 @@
+package cloud.ptl.boardgamecollector.io.dto;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UserGamesDTO extends AbstractDTO{
+    List<GameDetailsDTO> games;
+}
---
+++ app/src/main/java/cloud/ptl/boardgamecollector/io/parser/UserGamesParser.java
@@ -0,0 +1,41 @@
+package cloud.ptl.boardgamecollector.io.parser;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import cloud.ptl.boardgamecollector.io.dto.GameDetailsDTO;
+import cloud.ptl.boardgamecollector.io.dto.UserGamesDTO;
+import cloud.ptl.boardgamecollector.io.network.GameDetailsFetchAsyncTask;
+import lombok.SneakyThrows;
+
+public class UserGamesParser extends AbstractParser<UserGamesDTO> {
+    @SneakyThrows
+    @Override
+    public UserGamesDTO parse(InputStream is) throws XmlPullParserException, IOException {
+        UserGamesDTO userGamesDTO = new UserGamesDTO();
+        userGamesDTO.setGames(new ArrayList<>());
+        XmlPullParser parser = Xml.newPullParser();
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(is, null);
+        while(parser.next() != XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("item"))
+                if(parser.getAttributeValue(null, "subtype").equals("boardgame")){
+                    Long id = Long.valueOf(parser.getAttributeValue(null, "objectid"));
+                    GameDetailsDTO gameDetailsDTO =
+                            new GameDetailsFetchAsyncTask().execute(id.toString()).get();
+                    userGamesDTO.getGames().add(gameDetailsDTO);
+                }
+        }
+        return userGamesDTO;
+    }
+}
